aaece5389ad851fc1b1f31baabd7192460e23ab0
Knight <mayanks365@gmail.com>
2021-09-12 22:50:01 +0530

    Movie Seat Booking local storage

--- MovieSeatBooking/script.js
+++ MovieSeatBooking/script.js
@@ -1,26 +1,60 @@
 const container = document.querySelector('.container');
-const seats = document.querySelectorAll('.seat .seat:not(.occupied');
+const seats = document.querySelectorAll('.row .seat:not(.occupied');
 const count = document.getElementById('count');
 const total = document.getElementById('total');
 const movieSelect = document.getElementById('movie');
 
 let ticketPrice = +movieSelect.value;
 
+//
+populateUI();
+
+// save selected movie index and price
+function setMovieData(movieIndex, moviePrice){
+    localStorage.setItem('selectedMovieIndex', movieIndex);
+    localStorage.setItem('seletedMoviePrice', moviePrice)
+}
 function updateSelectedCount(){
-    const selectedSeats = document.querySelectorAll('.row .seat.selected')
+    const selectedSeats = document.querySelectorAll('.row .seat.selected');
+    
+    const seatsIndex = [...selectedSeats].map( (seat) =>{
+        return [...seats].indexOf(seat);
+    });
+
+   // saving the list to local storage
+   localStorage.setItem('selectedSeats', JSON.stringify(seatsIndex));
 
     const selectedSeatsCount = selectedSeats.length;
 
     count.innerText = selectedSeatsCount;
     total.innerText = selectedSeatsCount * ticketPrice;
-}
+};
 
+// get Data from local storage and populate UI
+function populateUI(){
+    const selectedSeats = JSON.parse(localStorage.getItem('selectedSeats'));
+    seats.forEach( (seat, index) => {
+
+        if( selectedSeats.indexOf(index) > -1 ){
+            seat.classList.add('selected')
+        }
+    })
+
+    const selectedMovieIndex = localStorage.getItem('selectedMovieIndex');
+    if(selectedMovieIndex!== null){
+        movieSelect.selectedIndex = selectedMovieIndex;
+    }
+};
 
 // movie select event
 movieSelect.addEventListener( 'change',  e => {
     ticketPrice = +e.target.value;
+
+    // selectIndex and value
+    setMovieData(e.target.selectedIndex, e.target.value);
+
     updateSelectedCount();
-})
+});
 
 // seat click event
 container.addEventListener( 'click', (e) =>{
@@ -30,5 +64,7 @@ container.addEventListener( 'click', (e) =>{
         e.target.classList.toggle('selected');
         updateSelectedCount();
     }
-})
+});
 
+//
+updateSelectedCount();
